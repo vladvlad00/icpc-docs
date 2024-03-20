51ce9a01347fc3e78b65303a6f33807eebc85e1e
vladvlad00 <teodorescu.vlad@yahoo.com>
2024-03-20 14:22:27 +0200

    euc 25 pag

--- content/various/rand.cpp
+++ content/various/rand.cpp
@@ -6,5 +6,4 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); // for int 
 //mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); // for long long - returns in [0, 2^64)
 //uniform_int_distribution<> uniform(A, B);
 //uniform_real_distribution<> uniform(A, B);
-// usage: rng()
-// usage: uniform(rng)
+// usage: rng(), uniform(rng)
